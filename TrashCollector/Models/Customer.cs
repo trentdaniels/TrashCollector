59a557562c0f99b6bacd659b27591b1a7ff8a183
Trenton Daniels <trentondaniels95@gmail.com>
2018-08-28 09:43:48 -0500

    created Customer class

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DayOfWeek PickupDay { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double MoneyOwed { get; set; }
+
+    }
+}
---
+++ TrashCollector/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrashCollector.Startup))]
+namespace TrashCollector
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRoles();
+        }
+
+        private void CreateRoles()
+        {
+            
+        }
+    }
+
+   
+}
